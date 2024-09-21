e3c2301b59f08122df7647b69b676b5b44e52c51
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-21 15:52:13 +0100

    datatypes and operators

---
+++ L2 - Operators and Datatypes/main.py
@@ -0,0 +1,26 @@
+name = "Moses" #string
+age = 18 #integers
+height = 5.2 #float
+is_student = True #boolean
+print(age)
+print("20" + "20")
+x = 20
+y = 20
+print(int(x) + int(y))
+name = bool(name)
+print(name)
+print(type(name))
+
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
+print (num1 - num2)
+print (num1 / num2)
+print (num1 * num2)
+print (num1 ** 2)
+print (num2 ** 0.5)
+print (num1 > num2)
+print (num1 < num2)
+print (num1 == num2)
+print (num1 != num2)
