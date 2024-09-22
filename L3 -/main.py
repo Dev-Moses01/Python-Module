53f3d8b7efced5d574ffe8d608251fbaa8d02a78
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-22 15:52:48 +0100

    Conditional Statement

---
+++ L3 -/main.py
@@ -0,0 +1,21 @@
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is an even number sir")
+else:
+    print("The number is an odd number sir")
+
+import datetime
+
+print(datetime.datetime.now())
+
+import calendar
+
+print(calendar.calendar(2024))
+
+age = 100
+if age < 20:
+ print("You are an Underage")
+elif age > 60:
+ print("You are Very Old")
+else:
+ print("You are an Adult")
