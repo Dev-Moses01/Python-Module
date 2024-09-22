9aa8a2ebe126720694df668dcdc9d8038ffb446f
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-22 16:09:51 +0100

    After class project

--- L3 - Conditional Statement/main.py
+++ L3 - Conditional Statement/main.py
@@ -1,21 +1,30 @@
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print("The number is an even number sir")
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("The number is an even number sir")
+# else:
+#     print("The number is an odd number sir")
+
+# import datetime
+
+# print(datetime.datetime.now())
+
+# import calendar
+
+# print(calendar.calendar(2024))
+
+# age = 100
+# if age < 20:
+#  print("You are an Underage")
+# elif age > 60:
+#  print("You are Very Old")
+# else:
+#  print("You are an Adult")
+
+#Assignment
+attendance = int(input("Enter the total number of working days: "))
+absent = int(input("Enter the total number of absent days: "))
+percent = attendance + absent/100
+if percent < 75:
+    print("You are not eligible to sit for the exam")
 else:
-    print("The number is an odd number sir")
-
-import datetime
-
-print(datetime.datetime.now())
-
-import calendar
-
-print(calendar.calendar(2024))
-
-age = 100
-if age < 20:
- print("You are an Underage")
-elif age > 60:
- print("You are Very Old")
-else:
- print("You are an Adult")
+    print("You are eligible to sit for the exam") 
