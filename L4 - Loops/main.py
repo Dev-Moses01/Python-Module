c261dc2dcb54b8c856e1c8ece6b4ae8c19c6f1c4
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-28 15:49:16 +0100

    Loops

---
+++ L4 - Loops/main.py
@@ -0,0 +1,10 @@
+for m in range(1,10+1):
+    print(5 * m)
+# for k in "Moses":
+#     print(k)
+
+fruits = ['apple', 'watermelon', 'strawberry']
+adj = ['sweet', 'juicy', 'tasty']
+for i in fruits:
+    for m in adj:
+        print(m,i)
