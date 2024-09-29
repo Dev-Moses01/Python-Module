249e6628c0e19022dbcfea55ed4691a165a3ccdd
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-29 16:10:03 +0100

    after-class project

--- L5 - Functions/main.py
+++ L5 - Functions/main.py
@@ -1,25 +1,37 @@
-age = 15
-def intro():
-    name = 'Moses'
-    print("I am Him " + name)
+# age = 15
+# def intro():
+#     name = 'Moses'
+#     print("I am Him " + name)
 
-def outro():
-    print("Goodbye Bwoi")
-    print(age)
+# def outro():
+#     print("Goodbye Bwoi")
+#     print(age)
 
-intro()
-outro()
+# intro()
+# outro()
 
-def add(x,y):
-    return x + y
-def multiply(x,y):
-    return x * y
-def divide(x,y):
-    return x / y
-def sub(x,y):
-    return x - y
+# def add(x,y):
+#     return x + y
+# def multiply(x,y):
+#     return x * y
+# def divide(x,y):
+#     return x / y
+# def sub(x,y):
+#     return x - y
 
-print(add(5,5))
-print(multiply(5,5))
-print(int(divide(5,5)))
-print(sub(5,5))
+# print(add(5,5))
+# print(multiply(5,5))
+# print(int(divide(5,5)))
+# print(sub(5,5))
+#Assignment
+
+def fibonacci_series(n):
+    series = []
+    a, b = 0, 1
+    for m in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+num_terms = int(input("Enter the number of terms:- "))
+print(fibonacci_series(num_terms))
