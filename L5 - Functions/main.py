490294866590920ae9ed1df3b1d16aa29a292d93
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-09-29 15:45:41 +0100

    function

---
+++ L5 - Functions/main.py
@@ -0,0 +1,25 @@
+age = 15
+def intro():
+    name = 'Moses'
+    print("I am Him " + name)
+
+def outro():
+    print("Goodbye Bwoi")
+    print(age)
+
+intro()
+outro()
+
+def add(x,y):
+    return x + y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+def sub(x,y):
+    return x - y
+
+print(add(5,5))
+print(multiply(5,5))
+print(int(divide(5,5)))
+print(sub(5,5))
