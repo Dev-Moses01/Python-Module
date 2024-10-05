384247fb88da99c7989f66531568dbfa604c1ae4
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-05 17:09:57 +0100

    after-class project

--- L6 - Turtle/main.py
+++ L6 - Turtle/main.py
@@ -6,7 +6,9 @@ screen.bgcolor("orange")
 screen.setup(400,400)
 
 t = turtle.Turtle()
+t.color("red")
 #forward, backward, left, right
+#square
 t.backward(100)
 t.right(90)
 t.backward(100)
@@ -14,6 +16,20 @@ t.right(90)
 t.backward(100)
 t.right(90)
 t.backward(100)
-
-
+#triangle
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
+#hexagon
+for _ in range(6):
+    t.forward(100)
+    t.left(60)
+#rectangle
+t.forward(70)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(70)
+t.right(90)
+t.forward(150)
 turtle.done()
