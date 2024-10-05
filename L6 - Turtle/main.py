2edb77fc1f49664f2113ce8cf79559c1da0e95d4
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-05 15:51:11 +0100

    square on turtle

---
+++ L6 - Turtle/main.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.title("My First Turtle")
+screen = turtle.Screen()
+screen.bgcolor("orange")
+screen.setup(400,400)
+
+t = turtle.Turtle()
+#forward, backward, left, right
+t.backward(100)
+t.right(90)
+t.backward(100)
+t.right(90)
+t.backward(100)
+t.right(90)
+t.backward(100)
+
+
+turtle.done()
