965eea1efedbd6c3561865020a26eacb552c1581
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-06 15:53:18 +0100

    data structures

---
+++ L7 - Data Structures/main.py
@@ -0,0 +1,26 @@
+name = ["Moses", "Kate", "Isreal", "David"]
+print(name)
+name.sort()
+print(name)
+name.reverse()
+print(name)
+name.append("Sonia")
+print(name)
+name.pop(3)
+print(name)
+numbers = [36, 12, 40, 7, 100, 2, 74]
+print(numbers)
+numbers.sort()
+print(numbers)
+
+student1 = {
+    "name": 'Moses',
+    "school": 'Codingal',
+    "about": 'Chill'
+}
+student1["name"] = 'Boss'
+student1["hobbies"] = 'Problem-solving'
+student1.pop("school")
+print(student1)
+print(student1["name"])
+
