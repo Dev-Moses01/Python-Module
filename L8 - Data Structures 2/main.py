f7dc5a8dfc46cd200392a29e1bc76f38e93952b5
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-12 15:55:01 +0100

    Tuples and Sets

---
+++ L8 - Data Structures 2/main.py
@@ -0,0 +1,14 @@
+my_list = [1,2,3,4,5]
+my_tuple = ("Moses", "Mose", "Mos", "Mo", "M")
+print(my_list)
+print(my_tuple)
+print(my_tuple[2:])
+print(my_tuple[:1])
+
+set1 = {1,2,3,4,5}
+set2 = {2,4,6,8,10}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
