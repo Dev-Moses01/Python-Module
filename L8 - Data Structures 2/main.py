ea4ad27c10bf1ce2dec0f5ef179d764dd572fd8b
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-12 16:25:18 +0100

    after-class project

--- L8 - Data Structures 2/main.py
+++ L8 - Data Structures 2/main.py
@@ -12,3 +12,9 @@ print(set1.union(set2))
 print(set1.intersection(set2))
 print(set1.difference(set2))
 print(set1.symmetric_difference(set2))
+
+#After class project
+my_list = list(my_tuple)
+print("Tuple: ", my_tuple)
+print("List: ", my_list)
+print(my_list.pop())
