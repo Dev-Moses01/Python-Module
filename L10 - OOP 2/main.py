b83078f8f2a590b14712f07890b2b98eb97f7847
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-10-19 16:05:17 +0100

    Destructor and Constructor

--- L10 - OOP 2/main.py
+++ L10 - OOP 2/main.py
@@ -11,10 +11,14 @@ class Library:
     def addBook(self, book):
         self.booklist.append(book)
         print(book, "is added successfully in: ", self.name)
+    
+    def __del__(self):
+        print("Object deleted successfully")
 
 library1 = Library(["Mathematics", "Physics", "Chemistry"], "Bookstore")
 library2 = Library(["Legacies", "Originals", "Teen wolves"], "Novels")
 
+del library1
 library1.addBook("Biology")
 library2.addBook("Van Helsing")
 
