95552afd70721361d00e639ccf58f2ea39f785a8
Dev-Moses01 <mosesakinrinade01@gmail.com>
2024-12-31 13:35:57 +0100

    After-class project

---
+++ L22 - Mathematical Magic 1/afterclass.py
@@ -0,0 +1,9 @@
+def binary_to_decimal():
+    binary_code = input("Enter a binary number (only 0s and 1s): ")
+    try:
+        decimal = int(binary_code, 2)
+        print(f"The decimal equivalent of binary {binary_code} is {decimal}.")
+    except ValueError:
+        print("Invalid binary number. Please enter a valid binary code (only 0s and 1s).")
+        
+binary_to_decimal()
