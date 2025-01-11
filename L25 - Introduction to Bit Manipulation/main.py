de2a777b3f73b7f5c1c7df9b27c24591b940d6c9
Dev-Moses01 <mosesakinrinade01@gmail.com>
2025-01-11 17:01:23 +0100

    Introduction to Bit Manipulation

---
+++ L25 - Introduction to Bit Manipulation/main.py
@@ -0,0 +1,10 @@
+print(bin(50))
+print(bin(70))
+
+print(bin(50 & 70))
+#AND (&) sets each bit to 1 if both are 1
+#OR (|) sets each bit to 1 if atleast one of the bits is one
+#XOR (^) sets the bit to 1 if the bits are different
+#NOT (~) bitwise negation == 1 becomes 0 and vice versa
+
+print(int("1111", 2))
