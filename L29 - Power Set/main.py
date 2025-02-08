7bf3cde7795490a21cb8e30d6cd8d243c5ddf7d0
Dev-Moses01 <mosesakinrinade01@gmail.com>
2025-02-08 15:41:59 +0100

    Flip bits

---
+++ L29 - Power Set/main.py
@@ -0,0 +1,13 @@
+def totalflip(number1, number2):
+    flips = 0
+    while(number1 > 0 or number2 > 0):
+        t1 = (number1 & 1)
+        t2 = (number2 & 1)
+        if (t1 != t2):
+            flips += 1
+        #right shift - remove the last digit
+        number1 >>= 1
+        number2 >>= 1
+    return flips
+
+print(totalflip(2, 10))
