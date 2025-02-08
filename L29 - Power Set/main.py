aac42fd2b039f56775b79713344843097b329a73
Dev-Moses01 <mosesakinrinade01@gmail.com>
2025-02-08 16:32:29 +0100

    Power Set

--- L29 - Power Set/main.py
+++ L29 - Power Set/main.py
@@ -11,3 +11,14 @@ def totalflip(number1, number2):
     return flips
 
 print(totalflip(2, 10))
+
+#Power Set
+def power_set_bitwise(s):
+    n = len(s)
+    power_set = []
+    for i in range(1 << n):  # 2^n subsets
+        subset = [s[j] for j in range(n) if (i & (1 << j)) > 0]
+        power_set.append(subset)
+    return power_set
+
+print(power_set_bitwise([1, 2, 3]))
