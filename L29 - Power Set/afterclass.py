128b67637181998cb97b9035146052323fdb2ffa
Dev-Moses01 <mosesakinrinade01@gmail.com>
2025-02-08 16:33:53 +0100

    Afterclass project

---
+++ L29 - Power Set/afterclass.py
@@ -0,0 +1,10 @@
+def find_all_substrings(s):
+    substrings = []
+    n = len(s)
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            substrings.append(s[i:j])
+    return substrings
+
+string = "abc"
+print(find_all_substrings(string))
